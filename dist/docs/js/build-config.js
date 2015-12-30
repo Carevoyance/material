9628ac48fb25b63a970def407b87943868deb643
Anatoly Geyfman <anatoly@geyfman.net>
2015-12-29 19:21:16 -0800

    fixed bug in backdrop handling

---
+++ dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.4.8",
+  "version": "1.0.1",
+  "repository": "https://github.com/angular/material",
+  "commit": "8ef798f0fe242c555ef7c11fa0abe762fb218a02",
+  "date": "2015-12-23 09:11:42 -0600"
+});
